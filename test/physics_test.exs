4520ad45b6270338ce2a7bf964f2c7bd086cf4f1
Ryan Yogan <ryanyogan@gmail.com>
2017-04-15 14:30:16 -0700

    refactored code a bit

--- test/physics_test.exs
+++ test/physics_test.exs
@@ -7,6 +7,16 @@ defmodule PhysicsTest do
     assert ev == 11.2
   end
 
+  test "escape velocity of mars is correct" do
+    ev = Physics.Rocketry.escape_velocity(:mars)
+    assert ev == 5.1
+  end
+
+  test "escape velocity of the moon is correct" do
+    ev = Physics.Rocketry.escape_velocity(:moon)
+    assert ev == 2.4
+  end
+
   test "escape velocity of planet X is correct" do
     ev = %{mass: 4.0e22, radius: 6.21e6}
       |> Physics.Rocketry.escape_velocity
